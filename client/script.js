c0c9722f15ac8cdf57cd62f45075365c23d1bb48
Thuso Ndou <thusondou30@gmail.com>
2023-04-10 02:31:41 +0200

    update the server url

--- client/script.js
+++ client/script.js
@@ -81,7 +81,7 @@ const handleSubmit = async (e) => {
    loader(messageDiv);
 
    // fetch data from server -> bot response
-  const response = await fetch('http://localhost:5000/',{
+  const response = await fetch('https://maps-ai.onrender.com/',{
     method: 'POST',
     headers: {'Content-Type':'application/json'},
     body: JSON.stringify({prompt: data.get('prompt')})
